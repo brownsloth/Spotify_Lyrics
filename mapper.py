30afd419c936f281bc5de69a023ca73be30814db
Willibrordus Bayu <willibayu28@gmail.com>
2022-12-31 15:56:55 +0700

    First Commit

--- mapper.py
+++ mapper.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-"""mapper.py"""
-
+# import python library
 import sys
 
 # input comes from STDIN (standard input)
@@ -11,9 +9,6 @@ for line in sys.stdin:
     words = line.split()
     # increase counters
     for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
+        # write the results to STDOUT (standard output)
         # tab-delimited; the trivial word count is 1
         print('%s\t%s' % (word, 1))
